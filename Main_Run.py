1b15ac011b699558d986acfe1cd89e0e61967bdf
mitsakos178 <76257091+mitsakos178@users.noreply.github.com>
2021-07-07 19:45:31 +0300

    Final_Changes

--- Main_Run.py
+++ Main_Run.py
@@ -10,7 +10,7 @@ def main():
     #        Reading the image from disc          #
     # ============================================#
 
-    image = cv2.imread("resources/house.png")
+    image = cv2.imread("resources/house.jpg")
     cv2.imshow("Original Image", image)
     cv2.waitKey(0)
 
@@ -23,7 +23,7 @@ def main():
     print("Gray scale image: ", imgGray.shape,len(imgGray.shape))
     cv2.imshow("Gray Image", imgGray)
     cv2.waitKey(0)
-    cv2.imwrite("/Results/Gray_Scale_Image.png", imgGray)
+    cv2.imwrite("Results/Gray_Scale_Image.png", imgGray)
 
     # ==========================================#
     # Adding Salt and Pepper noise to image     #
@@ -32,7 +32,7 @@ def main():
     SPnoise_img = myImNoise(imgGray, param="saltandpepper")
     cv2.imshow('Salt And Pepper Noise Image', SPnoise_img)
     cv2.waitKey(0)
-    cv2.imwrite('/Results/Salt_And_Pepper_Noise_Image.png', SPnoise_img)
+    cv2.imwrite('Results/Salt_And_Pepper_Noise_Image.png', SPnoise_img)
 
     # =======================================#
     #      Adding noise Gaussian to image    #
@@ -41,7 +41,7 @@ def main():
     Gnoise_img = myImNoise(imgGray, param="gaussian")
     cv2.imshow('Gaussian Noise Image ', Gnoise_img)
     cv2.waitKey(0)
-    cv2.imwrite('/Results/Gaussian_Noise_Image.png', Gnoise_img)
+    cv2.imwrite('Results/Gaussian_Noise_Image.png', Gnoise_img)
 
     # =======================================#
     #      Mean filter Gaussian noise image  #
@@ -50,7 +50,7 @@ def main():
     Mean = myImFilter(Gnoise_img, param="mean")
     cv2.imshow('Mean filtered Image ', Mean)
     cv2.waitKey(0)
-    cv2.imwrite('/Results/Mean_filtered_Image.png', Mean)
+    cv2.imwrite('Results/Mean_filtered_Image.png', Mean)
 
     # =======================================#
     #   Median filter S&P noise image        #
@@ -58,7 +58,7 @@ def main():
     Median = myImFilter(SPnoise_img, param="median")
     cv2.imshow('Median filtering Image ', Median)
     cv2.waitKey(0)
-    cv2.imwrite('/Results/Median_filtering_Image.png', Median)
+    cv2.imwrite('Results/Median_filtering_Image.png', Median)
 
 if __name__ == "__main__":
     main()
